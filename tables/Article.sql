07e6f345b875db3a2f96b7f81f407facaa12b805
Janek Leuderalbert <leuderalbert.j@gmail.com>
2019-03-29 12:53:34 +0100

    fixed type and added default for IsBooking

--- tables/Article.sql
+++ tables/Article.sql
@@ -7,43 +7,43 @@ CREATE TABLE Article
   Name        VARCHAR(255) NOT NULL,
   Color       VARCHAR(255),
   Type        VARCHAR(255),
-  IsBooking   TINYINT(1)   NOT NULL,
+  IsBooking   TINYINT(1)   NOT NULL DEFAULT 1,
   VehicleType VARCHAR(255) NOT NULL,
   PRIMARY KEY (ID)
 );
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('Tür Rechts', 'rot', 'Karosserie', 'Golf2', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('Tür Rechts', 'rot', 'Karosserie', 'Golf2');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('OMP TRS', 'swarz', 'Sitz', 'Zubehör', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('OMP TRS', 'schwarz', 'Sitz', 'Zubehör');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('Momo GT', 'schwarz', 'Lenkrad', 'Zubehör', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('Momo GT', 'schwarz', 'Lenkrad', 'Zubehör');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('OEM Standard', 'N/A', 'Auspuff', 'BMW3er', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('OEM Standard', 'N/A', 'Auspuff', 'BMW3er');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('OEM Standard', 'Braun', 'Lenkrad', 'BMW3er', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('OEM Standard', 'Braun', 'Lenkrad', 'BMW3er');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('OEM Standard', 'N/A', 'Zylinderkopf', 'OpelKadett', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('OEM Standard', 'N/A', 'Zylinderkopf', 'OpelKadett'1);
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('Tuning Super Light', 'N/A', 'Kolben', 'Golf2', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('Tuning Super Light', 'N/A', 'Kolben', 'Golf2');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('Kotflügel', 'rot', 'Karosserie', 'Golf2', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('Kotflügel', 'rot', 'Karosserie', 'Golf2');
 
 INSERT INTO Article
-  (Name, Color, Type, VehicleType, IsBooking)
-VALUES ('Leuchte vorne links', 'N/A', 'Karosserie', 'BMWer', 1);
+  (Name, Color, Type, VehicleType)
+VALUES ('Leuchte vorne links', 'N/A', 'Karosserie', 'BMWer');

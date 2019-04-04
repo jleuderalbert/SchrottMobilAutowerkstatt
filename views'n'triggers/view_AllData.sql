d669ccb74969f97620f9deaf4ee282f9cb8531be
Janek Leuderalbert <leuderalbert.j@gmail.com>
2019-04-04 08:12:30 +0200

    changed name of id column

--- views'n'triggers/view_AllData.sql
+++ views'n'triggers/view_AllData.sql
@@ -4,5 +4,5 @@ DROP VIEW IF EXISTS AllData;
 CREATE VIEW AllData AS (
   SELECT *
   FROM OrderOverview
-         LEFT JOIN ResolveOrderItem OI ON OrderOverview.ID = OI.RepairOrderID
+         LEFT JOIN ResolveOrderItem OI ON OrderOverview.OrderNumber = OI.RepairOrderID
 );
